acab48522dffeca93bfa02ebc1f26be6982ada45
mohamedamr77 <moamr947@gmail.com>
2025-03-10 14:09:42 +0200

    dsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print("fsafas");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
